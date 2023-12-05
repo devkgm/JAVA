34246dedb7a02dfb2b4f98b520216cc4cd224673
devkgm <gm.kim333@gmail.com>
2023-12-05 11:21:21 +0900

    first commit

---
+++ src/com/devkim/study1/Study1.java
@@ -0,0 +1,27 @@
+package com.devkim.study1;
+
+import java.util.Scanner;
+
+public class Study1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("정수 하나를 입력하세요");
+		
+		int num = sc.nextInt();
+		
+//		입력받은 정수의 약수를 출력하세요.
+		for(int j = 1; j <= 100; j++) {
+			System.out.println(j);
+			for(int i = 1; i<= j /2; i++) {
+				if (j % i == 0) {
+					System.out.print(i+" ");
+				}
+			}
+			System.out.println(j);
+
+		}
+	}
+
+}
