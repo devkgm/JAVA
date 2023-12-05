7fa104c094739c1f86b6ceb0f0046750b4565703
devkgm <gm.kim333@gmail.com>
2023-12-05 16:41:06 +0900

    mmorpg

--- src/com/devkim/study1/Study4.java
+++ src/com/devkim/study1/Study4.java
@@ -1,46 +1,90 @@
 package com.devkim.study1;
 
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.util.Scanner;
 
 public class Study4 {
+    static int exp = 0;
+    static int level = 1;
+    static int gold = 0;
     public static void main(String[] args) {
-        // fps
-        // bullet : 30 * 3
-        Scanner sc = new Scanner(System.in);
-        //1. 단발
-        //2. 점사
-        //3. 연사
-        int bullet = 90;
-        for(int i = 0; i < 3; i++){
-            System.out.println("발사 모드를 선택하세요.");
-            System.out.println("1. 단발 2. 점사 3. 연사");
-            
-            int mode;
-            mode = sc.nextInt();
-            switch (mode) {
-                case 1:
-                    for(int j = 0; j < 30; j++){
-                        System.out.println("탕");
-                    }
-                    break;
-                case 2:
-                    for(int j = 0; j < 10; j++){
-                        System.out.println("탕탕탕");
-                    }
-                    break;
-                case 3:
-                    for(int j = 0; j < 30; j++){
-                            System.out.print("탕");
-                    }
-                    System.out.println("");
-                    break;
-                default:
-                    break;
+        login();
+        while(true){
+            monster();
+            levelUp();
+            state();
+            if (level%5 == 0 && endCheck()) {
+                break;
             }
-            System.out.println("장전");
         }
-        System.out.println("탄을 모두 소모했습니다.");
+        state();
+    }
+    public static void login(){
+        Scanner sc = new Scanner(System.in);
+        String id = "user01";
+        String pwd = "1234";
+        System.out.println("로그인 해주세요.");   
+        System.out.println("아이디 :");   
+        String input_id = sc.nextLine();
+        System.out.println("비밀번호 :");
+        String input_pwd = sc.nextLine();
+        System.out.println(id+input_id+pwd+input_pwd);
+        if(id.equals(input_id) && pwd.equals(input_pwd)){
+            System.out.println("로그인에 성공했습니다.");
+        } else {
+            System.out.println("로그인에 실패했습니다.");
+        }
+    }
+    public static void monster(){
+        Scanner sc = new Scanner(System.in);
+        int monster_num =(int) (Math.random()*3 + 1);
+        System.out.println("몬스터 "+monster_num+" 마리가 출몰했습니다.");
+        System.out.println("1. 사냥한다 2. 도망친다.");
+        int mode = sc.nextInt();
+        if(mode == 1){
+            System.out.println("몬스터 "+monster_num+"마리를 사냥했습니다!");
+            int addGold = (int)(Math.random()*100) + 1;
+            System.out.println(addGold+"골드 획득!");
+            gold += addGold;
+            exp += monster_num;
+        } else {
+            System.out.println("도망쳤습니다!");
+        }
+    }
+    public static void levelUp(){
+        if(level == exp/3){
+            level++;
+            System.out.println("레벨업!");
+        }
+        switch (level) {
+            case 5:
+                gold += 1000;
+                System.out.println("5 레벨업 보상 1000골드 획득");
+                break;
+            case 10:
+                gold += 2000;
+                System.out.println("10 레벨업 보상 2000골드 획득");
+                break;
+            case 15:
+                gold += 3000;
+                System.out.println("15 레벨업 보상 3000골드 획득");
+                break;
+            default:
+                break;
+        }
+    }
+    public static void state(){
+        System.out.println("현제 레벨 : "+level);
+        System.out.println("현제 보유 골드 : "+ gold);
+    }
+    public static boolean endCheck(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("종료하시겠습니까?");
+        System.out.println("1. 예 2. 아니요");
+        int exit = sc.nextInt();
+        if(exit == 1){
+            return true;
+        } else {
+            return false;
+        }
     }
 }
