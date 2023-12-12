8d00af5f7624155ab00da3a9e1f500f97226ee52
devkgm <gm.kim333@gmail.com>
2023-12-12 09:40:44 +0900

    magition

---
+++ src/com/devkim/rpg/Magition.java
@@ -0,0 +1,11 @@
+package com.devkim.rpg;
+
+public class Magition {
+    String name;
+    int hp;
+    int mp;
+    public void info(){
+        
+        System.out.println("이름: "+name+" 체력: "+hp+" 마나: "+mp);
+    }
+}
---
+++ src/com/devkim/rpg/RpgMain.java
@@ -0,0 +1,11 @@
+package com.devkim.rpg;
+
+public class RpgMain {
+    public static void main(String[] args) {
+        Magition magition = new Magition();
+        magition.name="winter";
+        magition.hp = 100;
+        magition.mp = 200;
+        magition.info();
+    }
+}
