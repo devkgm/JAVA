f0f55d7022ef11f6b05800f6733a5787e9802584
devkgm <gm.kim333@gmail.com>
2023-12-13 15:20:11 +0900

    Card

---
+++ src/com/devkim/card/Card.java
@@ -0,0 +1,50 @@
+package com.devkim.card;
+
+public class Card {
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private String companyName;
+    private String job;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return this.phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getCompanyName() {
+        return this.companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getJob() {
+        return this.job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+}
